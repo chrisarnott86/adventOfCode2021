4e87809b446af2acfa09b3e7f6e94c6cb11c7ea3
Your Name <you@example.com>
2021-12-08 14:55:03 +0000

    very nearly there

--- 8_2.py
+++ 8_2.py
@@ -14,38 +14,89 @@ for i in temp:
     config.append(i.split(' | ')[0])
 
 lookup = dict()
-for vals in config:
+for i,vals in enumerate(config):
     for conf in vals.split(' '):
         letset = set()
-        for i in conf:
-            letset.add(i)
+        for j in conf:
+            letset.add(j)
         if len(conf)==2:
             lookup[1] = letset
-        elif len(conf)==4:
+    for conf in vals.split(' '):
+        letset = set()
+        for j in conf:
+            letset.add(j)
+        if len(conf)==4:
             lookup[4] = letset
-        elif len(conf)==3:
+    for conf in vals.split(' '):
+        letset = set()
+        for j in conf:
+            letset.add(j)
+        if len(conf)==3:
             lookup[7] = letset
-        elif len(conf)==7:
+    for conf in vals.split(' '):
+        letset = set()
+        for j in conf:
+            letset.add(j)
+        if len(conf)==7:
             lookup[8] = letset
-        elif len(conf)==6 and lookup[7] not in letset:
+    for conf in vals.split(' '):
+        letset = set()
+        for j in conf:
+            letset.add(j)
+        if len(conf)==6 and not lookup[7].issubset(letset):
             lookup[6] = letset
-        elif len(conf)==5 and lookup[7] in letset:
-            lookup[3] = letset  
-        elif len(conf)==5 and (lookup[3]+lookup[4])-lookup[1] in letset:
-            lookup[5] = letset 
-        elif len(conf)==5 and lookup[8]-(lookup[3]+lookup[5]) in letset:
+    for conf in vals.split(' '):
+        letset = set()
+        for j in conf:
+            letset.add(j)
+        if len(conf)==5 and lookup[7].issubset(letset):
+            lookup[3] = letset
+    for conf in vals.split(' '):
+        letset = set()
+        for j in conf:
+            letset.add(j)
+        if len(conf)==5 and ((lookup[3]|lookup[4])-lookup[1]).issubset(letset):
+            lookup[5] = letset
+    for conf in vals.split(' '):
+        letset = set()
+        for j in conf:
+            letset.add(j)
+        if len(conf)==5 and (lookup[8]-(lookup[3]|lookup[5])).issubset(letset):
             lookup[2] = letset
-        elif len(conf)==6 and lookup[2]-lookup[3] in letset:
+    for conf in vals.split(' '):
+        letset = set()
+        for j in conf:
+            letset.add(j)
+        if len(conf)==6 and (lookup[2]-lookup[3]).issubset(letset):
             lookup[0] = letset
-        else:
+    for conf in vals.split(' '):
+        letset = set()
+        for j in conf:
+            letset.add(j)
+        if len(conf)==6 and (lookup[4]).issubset(letset):
             lookup[9] = letset
-
-
-print(lookup)
-count=0
-for out in output:
-    for val in out.split(' '):
-        if (len(val)==2 or len(val)==4 
-        or len(val)==3 or len(val)==7):
-            count+=1
-print(count)
+    stringnum=''
+    #print(i)
+    print(len(output[i].split(' ')))
+    for string in output[i].split(' '):
+        #print(string)
+        newset = set()
+        for letter in string:
+            newset.add(letter)
+        #for val in string.split(' '):
+        for key,combi in lookup.items():
+            if combi==newset:
+                stringnum+=str(key)
+    #print(lookup)
+    print(stringnum)
+#print(lookup)
+#count=0
+#for out in output:
+#    stringnum = ''
+#    for val in out.split(' '):
+#        for key,combi in lookup.items():
+#            tempset = { i for i in combi }
+#            if tempset == combi:
+#                stringnum+=str(key)
+#    print(stringnum)
+#print(count)
