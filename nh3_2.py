b9103f0d348dcafd5f3a8c92b17db75f84afe29f
Your Name <you@example.com>
2021-12-17 22:15:41 +0000

    nathan's 3_2 working (ish)

--- nh3_2.py
+++ nh3_2.py
@@ -112,45 +112,22 @@ def result_print_2(final_pos):
 
 
 # Function to check binary numbers for C02
-def c02_checks():
-    # Clear out Reusable Variables (but keep 'first_bits' etc as those values remain same:
-
-    Pos1 = []
-    Pos2 = []
-    Pos3 = []
-    Pos4 = []
-    Pos5 = []
-    Pos6 = []
-    Pos7 = []
-    Pos8 = []
-    Pos9 = []
-    Pos10 = []
-    Pos11 = []
-    Pos12 = []
-
-    def c02_check_loop(this_list, pos_var, bit_list, position):
-        #print("Values Kept for Pos1:", position_check_2(this_list, pos_var, bit_list, position))
-        position_check_2(this_list, pos_var, bit_list, position)
-        if len(pos_var) == 1:
-            global c02_scrub_rating
-            c02_scrub_rating = result_print_2(pos_var)
-            print("C02 Scrubber Rating Is:", c02_scrub_rating)
-
-
-    c02_check_loop(file_list, Pos1, first_bits, 0)
-    c02_check_loop(Pos1, Pos2, second_bits, 1)
-    c02_check_loop(Pos2, Pos3, third_bits, 2)
-    c02_check_loop(Pos3, Pos4, fourth_bits, 3)
-    c02_check_loop(Pos4, Pos5, fifth_bits, 4)
-    c02_check_loop(Pos5, Pos6, sixth_bits, 5)
-    c02_check_loop(Pos6, Pos7, seventh_bits, 6)
-    c02_check_loop(Pos7, Pos8, eighth_bits, 7)
-    c02_check_loop(Pos8, Pos9, ninth_bits, 8)
-    c02_check_loop(Pos9, Pos10, tenth_bits, 9)
-    c02_check_loop(Pos10, Pos11, eleventh_bits, 10)
-    c02_check_loop(Pos11, Pos12, twelfth_bits, 11)
-
-
+#def c02_checks(this_list, pos_var, bit_list, position):
+#    #print("Values Kept for Pos1:", position_check_2(this_list, pos_var, bit_list, position))
+#    position_check_2(this_list, pos_var, bit_list, position)
+#    if len(pos_var) == 1:
+#        global c02_scrub_rating
+#        c02_scrub_rating = result_print_2(pos_var)
+#        print("C02 Scrubber Rating Is:", c02_scrub_rating)
+            
+def c02_checks(this_list, pos_var, bit_list, position):
+    # Initially this code runs to check C02 Binary Numbers.
+    #print("Values Kept for Pos1:", position_check(this_list, pos_var, bit_list, position))
+    position_check_2(this_list, pos_var, bit_list, position)
+    if len(pos_var) == 1:
+        global c02_scrub_rating_final
+        c02_scrub_rating_final = result_print(pos_var)
+        print("C02 scrubbing Rating Is:", c02_scrub_rating_final)
 
 
 def oxygen_checks(this_list, pos_var, bit_list, position):
@@ -161,7 +138,6 @@ def oxygen_checks(this_list, pos_var, bit_list, position):
         global ox_gen_rating_final
         ox_gen_rating_final = result_print(pos_var)
         print("Oxygen Gen Rating Is:", ox_gen_rating_final)
-
 
 oxygen_checks(file_list, Pos1, first_bits, 0)
 oxygen_checks(Pos1, Pos2, second_bits, 1)
@@ -176,7 +152,44 @@ oxygen_checks(Pos9, Pos10, tenth_bits, 9)
 oxygen_checks(Pos10, Pos11, eleventh_bits, 10)
 oxygen_checks(Pos11, Pos12, twelfth_bits, 11)
 
-c02_checks()
+
+Pos1 = []
+Pos2 = []
+Pos3 = []
+Pos4 = []
+Pos5 = []
+Pos6 = []
+Pos7 = []
+Pos8 = []
+Pos9 = []
+Pos10 = []
+Pos11 = []
+Pos12 = []
+first_bits = []
+second_bits = []
+third_bits = []
+fourth_bits = []
+fifth_bits = []
+sixth_bits = []
+seventh_bits = []
+eighth_bits = []
+ninth_bits = []
+tenth_bits = []
+eleventh_bits = []
+twelfth_bits = []
+
+c02_checks(file_list, Pos1, first_bits, 0)
+c02_checks(Pos1, Pos2, second_bits, 1)
+c02_checks(Pos2, Pos3, third_bits, 2)
+c02_checks(Pos3, Pos4, fourth_bits, 3)
+c02_checks(Pos4, Pos5, fifth_bits, 4)
+c02_checks(Pos5, Pos6, sixth_bits, 5)
+c02_checks(Pos6, Pos7, seventh_bits, 6)
+c02_checks(Pos7, Pos8, eighth_bits, 7)
+c02_checks(Pos8, Pos9, ninth_bits, 8)
+c02_checks(Pos9, Pos10, tenth_bits, 9)
+c02_checks(Pos10, Pos11, eleventh_bits, 10)
+c02_checks(Pos11, Pos12, twelfth_bits, 11)
 
 # Final Answer:
 print("Oxygen Rating X C02 Scrubber Rating:", ox_gen_rating_final * c02_scrub_rating)
